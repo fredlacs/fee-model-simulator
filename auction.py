b0503606cddd6fb36bd000ee48242614abc002df
Fred <fredlacs@gmail.com>
2020-01-14 14:02:58 +0000

    Done basic auction execution

--- auction.py
+++ auction.py
@@ -1,21 +1,45 @@
 # Define auction
 # Author: Frederico Lacs
 
-class AuctionMechanism:
+class AuctionState:
+    """
+    Defines state of an auction.
+    What are the current bids?
+    What information about the state each agent has access to?
+    """
+
+    def __init__(self, prev=None): 
+        self.bids = prev.bids if prev else {}
+
+    def getVisibleBids(self, agent):
+        """
+        Return information agent has access to from the state (ie other bids and their timestamps)
+        Not every agent has access to the same information on the state
+        """
+        # TODO: remove this error and do logic for state information
+        if not agent: raise NotImplementedError("No agent supplied")
+        return self.bids
+    
+    def addBid(self, bidder, bid):
+        self.bids[bidder].append(bid)
+
+
+class AuctionMechanism(AuctionState):
     """
     Defines rules of how the auction mechanism works.
     Which bids win, how much should they pay?
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, prev=None):
+        AuctionState.__init__(self, prev)
 
     def allocationRule(self, auctioneers, bids, slots):
         """
-        Selects first auctioneer from list and queries for the allocation.
+        Selects auctioneer to execute the auction then queries
+        for its allocation rule.
         Expects auctioneer to be AbstractAuctioneerAgent
         """
-        return auctioneers[0].selectWinningBids(bids, slots)
+        return auctioneers[0].selectWinningBids(self=auctioneers[0], bids=bids, slots=slots)
 
     def paymentRule(self, bids):
         """
@@ -24,32 +48,23 @@ class AuctionMechanism:
         """
         raise NotImplementedError("Not implemented")
 
-
-class AuctionState:
-    """
-    Defines state of current auction. What are the current bids?
-    What information about the state each agent has access to?
-    """
-
-    def __init__(self, prev=None): 
-        self.bids = prev.bids if prev else {}
-
-    def getStateInformation(self, agent):
+    def executeAuctionRound(self, auctioneers, bidders, slots):
         """
-        Return information agent has access to from the state (ie other bids and their timestamps)
-        Not every agent has access to the same information on the state
+        Executes the end to end flow of a single auction and returns
+        dictionary of payments for each bidder to pay
         """
-        raise NotImplementedError("Not implemented")
+        bids = [ bidder.getBid(self.getVisibleBids(bidder)) for bidder in bidders ]
+        winningBids = self.allocationRule(auctioneers, bids, slots)
+        return self.paymentRule(winningBids)
 
 
-class FirstPriceAuction(AuctionMechanism, AuctionState):
+class FirstPriceAuction(AuctionMechanism):
     """
     First price auction mechanism
     """
 
     def __init__(self, prev=None):
-        AuctionMechanism.__init__(self)
-        AuctionState.__init__(self, prev)
+        AuctionMechanism.__init__(self, prev)
 
     def paymentRule(self, bids):
         """
@@ -58,14 +73,13 @@ class FirstPriceAuction(AuctionMechanism, AuctionState):
         return bids
 
 
-class SecondPriceAuction(AuctionMechanism, AuctionState):
+class SecondPriceAuction(AuctionMechanism):
     """
     Second price auction mechanism
     """
 
     def __init__(self, prev=None):
-        AuctionMechanism.__init__(self)
-        AuctionState.__init__(self, prev)
+        AuctionMechanism.__init__(self, prev)
 
     def paymentRule(self, bids):
         """
@@ -74,6 +88,7 @@ class SecondPriceAuction(AuctionMechanism, AuctionState):
         # sort dictionary from highest to lowest bid
         # sortedBidders = sorted(bids, key=bids.get, reverse=True)
         # shift every bid to the one lower
+        # use iter's __next__
         # return sortedBidders
         return super().paymentRule(bids)
 
--- simulator.py
+++ simulator.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python3
 # Author: Frederico Lacs
 
-from agents import FixedBidAgent
+from agents import FixedBidAgent, NaiveAuctioneerAgent
+from auction import FirstPriceAuction
 
-def runSimulation(numIterations):
+def runAuctions(numIterations):
+    auction = FirstPriceAuction()
+    auctioneers = [ NaiveAuctioneerAgent ]
+    bidders = [ FixedBidAgent(5),FixedBidAgent(3), FixedBidAgent(7) ]
+
     for i in range(numIterations):
-        print(f"Executing auction number {i}")
+        print(f"Executing auction number {i+1}")
+        results = auction.executeAuctionRound(auctioneers, bidders, 10)
+        print(results)
 
 
 if __name__ == '__main__':
     print("starting simulations")
 
     numIterations = 5
-    runSimulation(numIterations)
+    runAuctions(numIterations)
 
-    # bidders = [ FixedBidAgent(5),FixedBidAgent(3), FixedBidAgent(7) ]
+    # bidders = [  ]
     # for bidder in bidders:
     #     print(bidder.getBid(None))
 
