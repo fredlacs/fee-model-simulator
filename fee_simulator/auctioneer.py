b8a72d64361c9cbe25ee34caccb02a9ad3a639d8
Fred <fredlacs@gmail.com>
2020-04-21 16:35:23 +0100

    Refactor old code

--- fee_simulator/auctioneer.py
+++ fee_simulator/auctioneer.py
@@ -6,33 +6,9 @@ from ortools.algorithms import pywrapknapsack_solver
 Agent that selects winning bids in an auction
 """
 
-class NaiveAuctioneerAgent():
-    """
-    Selects allocation rule naively, without optimising revenue
-    """
-
-    def __init__(self):
-        pass
-    
-    def get_allocation_rule(self, bids, weightLimit):
-        """
-        Return winning bids.
-        This is the allocation rule.
-
-        Selects first 3 bids as winning bids
-        """
-        # naive approach w/o gas:
-        # sort dictionary from highest to lowest bid then select n winners
-        # winningBids = sorted(bids, key=bids.get, reverse=True)[:self.slotsPerAuction]
-        # return {winner: bids[winner] for winner in winningBids}
-
-        # take into account bid weights when selecting winning bids
-        return bids[:3]
-
-
 class KnapsackAuctioneerAgent():
     """
-    Knapsack optimisation on bids, currently with weight 1 on each bid
+    Knapsack optimisation on bids
     """
 
     def __init__(self):
@@ -40,7 +16,7 @@ class KnapsackAuctioneerAgent():
 
     def get_allocation_rule(self, bids, weight_limit):
         """
-        Selects highest bids
+        Selects highest bids using branch and bound optimisation technique
         """
         values = []
         weights = [[]]
