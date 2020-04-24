79b6bc8ed4a295d83811d46223ad10b61c99da8e
Fred <fredlacs@gmail.com>
2020-04-24 19:49:19 +0100

    Fix test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,20 +1,20 @@
-# The simulator tests are our integration tests
+# Integration tests!
 
 from click.testing import CliRunner
-from fee_simulator.simulator import run_auctions
+from fee_simulator.cli import run
 
 
 def test_different_simulation_numbers():
   runner = CliRunner()
-  result = runner.invoke(run_auctions, ['5'])
+  result = runner.invoke(run, ['5'])
   assert result.output == "Executing auction number 1\nExecuting auction number 2\nExecuting auction number 3\nExecuting auction number 4\nExecuting auction number 5\n"
 
-  result = runner.invoke(run_auctions, ['1'])
+  result = runner.invoke(run, ['1'])
   assert result.output == "Executing auction number 1\n"
 
 def test_csv_output():
   runner = CliRunner()
-  result = runner.invoke(run_auctions, ['1', '--outputfile', 'resultsTest.csv'])
+  result = runner.invoke(run, ['1', '--outputfile', 'resultsTest.csv'])
   assert result.output == "Executing auction number 1\n"
 
   file = open("resultsTest.csv", 'r', newline='')
